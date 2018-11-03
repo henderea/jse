f8f159274c84dcf650b1e8f7714def59324613f9
Eric Henderson <henderea@gmail.com>
2018-11-03 19:50:33 -0400

    working on switch to commander

--- bin/jse.js
+++ bin/jse.js
@@ -1,10 +1,32 @@
 #!/usr/bin/env node
-const yargs = require('yargs');
+// const yargs = require('yargs');
 const path = require('path');
-const argv = yargs
-    .usage('Usage: $0 command [args]')
-    .wrap(120)
-    .commandDir(path.join(__dirname, '../lib/commands'))
-    .help('h')
-    .alias('h', 'help')
-    .argv;
+const Command = require('@henderea/commander').Command;
+const program = new Command();
+const test = new Command();
+test
+    .unknownOptionAsArg(true)
+    .command('test2 <cmd> [args...]')
+    .action((cmd, args) => {
+        console.log('test2', cmd, args);
+    });
+test
+    .command('test3 <cmd> [args...]')
+    .action((cmd, args) => {
+        console.log('test3', cmd, args);
+    });
+
+program
+    .version(require(path.join(__dirname, '../package.json')).version)
+    .unknownOptionAsArg(true)
+    .command('test <cmd> [args...]', 'test command', { exec: test });
+
+program.parse(process.argv);
+
+// const argv = yargsy
+//     .usage('Usage: $0 command [args]')
+//     .wrap(120)
+//     .commandDir(path.join(__dirname, '../lib/commands'))
+//     .help('h')
+//     .alias('h', 'help')
+//     .argv;
