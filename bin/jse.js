5b13484101ad31defdba896e43c938eee20d5df8
Eric Henderson <henderea@gmail.com>
2018-12-03 14:18:20 -0500

    bundle the dependencies in

--- bin/jse.js
+++ bin/jse.js
@@ -1,10 +1,14 @@
-#!/usr/bin/env node
 const yargs = require('yargs');
 const path = require('path');
 const argv = yargs
     .usage('Usage: $0 command [args]')
     .wrap(120)
-    .commandDir(path.join(__dirname, '../lib/commands'))
+    .command(require('../lib/commands/about'))
+    .command(require('../lib/commands/auth'))
+    .command(require('../lib/commands/sudo'))
+    .command(require('../lib/commands/test_auth'))
+    .command(require('../lib/commands/unauth'))
+    .command(require('../lib/commands/var'))
     .help('h')
     .alias('h', 'help')
     .argv;
