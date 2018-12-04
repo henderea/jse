91e1f00b4ab688f69a7ebd1773be561dc230e919
Eric Henderson <henderea@gmail.com>
2018-12-04 11:44:12 -0500

    change some things around

--- bin/jse.js
+++ bin/jse.js
@@ -1,13 +1,2 @@
-const yargs = require('yargs');
-const path = require('path');
-const argv = yargs
-    .usage('Usage: $0 command [args]')
-    .wrap(120)
-    .command(require('../lib/commands/about'))
-    .command(require('../lib/commands/auth'))
-    .command(require('../lib/commands/sudo'))
-    .command(require('../lib/commands/test_auth'))
-    .command(require('../lib/commands/unauth'))
-    .command(require('../lib/commands/var'))
-    .help('help')
-    .argv;
+#!/usr/bin/env node
+require('..');
