90b5a4b5b3d028092f43f16163c3c0ff928942b7
Eric Henderson <henderea@gmail.com>
2021-08-21 12:10:42 -0400

    add a default export to the api

--- src/api.mjs
+++ src/api.mjs
@@ -1,2 +1,8 @@
-export { vars, state, shell, version } from '../lib/helpers/api.mjs';
+import jse, { vars, state, shell, version } from '../lib/helpers/api.mjs';
+
+export {
+  vars, state, shell, version
+};
+
+export default jse;
 
