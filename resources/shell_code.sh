ccf9267e2dd20a566a33777fab3fa1cdd6b0a688
Eric Henderson <henderea@gmail.com>
2022-05-18 11:49:33 -0400

    add shell integration

---
+++ resources/shell_code.sh
@@ -0,0 +1,63 @@
+function __jse_check {
+  if which jse >/dev/null 2>/dev/null; then
+    return 0
+  elif which nvm >/dev/null 2>/dev/null; then
+    if which nvm_find_nvmrc >/dev/null 2>/dev/null; then
+      if [[ -f "$(nvm_find_nvmrc)" ]]; then
+        nvm use >/dev/null 2>/dev/null
+      else
+        nvm use default >/dev/null 2>/dev/null
+      fi
+    else
+      nvm use default >/dev/null 2>/dev/null
+      nvm use >/dev/null 2>/dev/null
+    fi
+    if which jse >/dev/null 2>/dev/null; then
+      return 0
+    else
+      return 1
+    fi
+  else
+    return 1
+  fi
+}
+
+function jvg {
+  jse var get "$1" 2>/dev/null || return $?
+}
+
+function jvgl {
+  jse var get --lines "$1" 2>/dev/null || return $?
+}
+
+function jvge {
+  jse var get --escaped "$1" 2>/dev/null || return $?
+}
+
+function jvgeu {
+  jse var get-all --escaped --unique --sort $@ 2>/dev/null || return $?
+}
+
+function jva {
+  printf "> jse var add $*\n"
+  jse var add $@ || return $?
+}
+
+function jvm {
+  printf "> jse var mod $*\n"
+  jse var mod $@
+}
+
+function jse {
+  if ! __jse_check >/dev/null 2>/dev/null; then
+    printf "$(tput bold)$(tput setaf 1)Unable to find jse script$(tput sgr0)\n"
+    return 1
+  fi
+  if [[ "$1" == 'reload' ]]; then
+    printf "$(tput dim)Reloading jse shell integration$(tput sgr0)\n"
+    eval "$(command jse shell)"
+  else
+    command jse $@
+    return $?
+  fi
+}
--- src/jse.mjs
+++ src/jse.mjs
@@ -2,6 +2,7 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
 import about from '../lib/commands/about.mjs';
+import shell from '../lib/commands/shell.mjs';
 import varCommand from '../lib/commands/var.mjs';
 
 yargs(hideBin(process.argv))
@@ -11,6 +12,7 @@ yargs(hideBin(process.argv))
   .usage('Usage: $0 command [args]')
   .wrap(120)
   .command(about)
+  .command(shell)
   .command(varCommand)
   .help('help')
   .argv;
